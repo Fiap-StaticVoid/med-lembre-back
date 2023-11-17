092d0a3fe4adf48dd451cecf05bd5848403646d2
Matheus Mendes <mattheus2015@yahoo.com.br>
2023-11-17 14:22:30 -0300

    feat(root): criando config basico do container

---
+++ servidor/config.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse("/docs")
